d52b85b126cebc6398c1b1ce17d5e47b92d52083
wheatwaves <wheatwaves@SenmatoMacBook-Pro.local>
2017-11-20 21:52:24 -0500

    fix pr bugs

--- train/experience.py
+++ train/experience.py
@@ -129,11 +129,11 @@ class Experience(object):
       from_zero = True
     end_frame_index = None
     if from_zero:
-      while not end_frame_index or end_frame_index + (self._reward_length - 1) >= len(self._frames):
+      while not end_frame_index or end_frame_index + (self._reward_length - 1) - self._top_frame_index >= len(self._frames):
         index = np.random.randint(len(self._zero_reward_indices))
         end_frame_index = self._zero_reward_indices[index]
     else:
-      while not end_frame_index or not end_frame_index-3-self._top_frame_index >= 0 or end_frame_index + (self._reward_length - 1) >= len(self._frames):
+      while not end_frame_index or not end_frame_index-3-self._top_frame_index >= 0 or end_frame_index + (self._reward_length - 1) - self._top_frame_index >= len(self._frames):
           index = np.random.randint(len(self._non_zero_reward_indices))
           end_frame_index = self._non_zero_reward_indices[index]
           end_frame_index = random.randrange(end_frame_index-(self._reward_length - 1), end_frame_index+1)
