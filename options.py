de94f5f8e68f835a426c392fcbfaf7b76cd80d73
Ontree <wangyicmh@163.com>
2017-11-29 17:01:43 -0500

    change default result dir

--- options.py
+++ options.py
@@ -22,14 +22,14 @@ def get_options(option_type):
   tf.app.flags.DEFINE_string("name", "tmp", "name used to store checkpoints and log")
   tf.app.flags.DEFINE_integer("reward_length", 1, "length of future reward for reward prediction")
   tf.app.flags.DEFINE_integer("hdd", 6, "hdd to store log")
-  tf.app.flags.DEFINE_string("checkpoint_dir", '/media/bighdd'+str(tf.app.flags.FLAGS.hdd)+'/minghai1/capstone/results/' + tf.app.flags.FLAGS.name + '/checkpoints', "checkpoint directory")
+  tf.app.flags.DEFINE_string("checkpoint_dir", '/media/bighdd'+str(tf.app.flags.FLAGS.hdd)+'/minghai1/capstone/results2/' + tf.app.flags.FLAGS.name + '/checkpoints', "checkpoint directory")
   # For training
   if option_type == 'training':
     tf.app.flags.DEFINE_integer("parallel_size", 8, "parallel hread size")
     tf.app.flags.DEFINE_integer("local_t_max", 20, "repeat step size")
     tf.app.flags.DEFINE_float("rmsp_alpha", 0.99, "decay parameter for rmsprop")
     tf.app.flags.DEFINE_float("rmsp_epsilon", 0.1, "epsilon parameter for rmsprop")
-    tf.app.flags.DEFINE_string("log_file", '/media/bighdd'+str(tf.app.flags.FLAGS.hdd)+'/minghai1/capstone/results/' + tf.app.flags.FLAGS.name , "log file directory")
+    tf.app.flags.DEFINE_string("log_file", '/media/bighdd'+str(tf.app.flags.FLAGS.hdd)+'/minghai1/capstone/results2/' + tf.app.flags.FLAGS.name , "log file directory")
     tf.app.flags.DEFINE_float("initial_alpha_low", 1e-4, "log_uniform low limit for learning rate")
     tf.app.flags.DEFINE_float("initial_alpha_high", 5e-3, "log_uniform high limit for learning rate")
     tf.app.flags.DEFINE_float("initial_alpha_log_rate", 0.5, "log_uniform interpolate rate for learning rate")
@@ -38,7 +38,7 @@ def get_options(option_type):
     tf.app.flags.DEFINE_float("entropy_beta", 0.001, "entropy regularization constant")
     tf.app.flags.DEFINE_float("pixel_change_lambda", 0.05, "pixel change lambda") # 0.05, 0.01 ~ 0.1 for lab, 0.0001 ~ 0.01 for gym
     tf.app.flags.DEFINE_integer("experience_history_size", 2000, "experience replay buffer size")
-    tf.app.flags.DEFINE_integer("max_time_step", 10 * 10**7, "max time steps")
+    tf.app.flags.DEFINE_integer("max_time_step", 100 * 10**7, "max time steps")
     tf.app.flags.DEFINE_integer("save_interval_step", 100 * 1000, "saving interval steps")
     tf.app.flags.DEFINE_boolean("grad_norm_clip", 40.0, "gradient norm clipping")
 
