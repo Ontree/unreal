79ca1736115dd0bee84ddce364e0c018b5e91f3f
Ontree <wangyicmh@163.com>
2017-12-02 15:02:15 -0500

    add loss to summary - fix bug1

--- train/trainer.py
+++ train/trainer.py
@@ -471,7 +471,7 @@ class Trainer(object):
     # Calculate gradients and copy them to global network.
     #sess.run( self.apply_gradients, feed_dict=feed_dict)
     if self.use_autoencoder and self.use_future_reward_prediction:
-      decoder_loss, frp_loss, value_loss, policy_loss, _ = sess.run( [self.decoder_loss, self.frp_loss, self.value_loss, self.policy_loss, self.apply_gradients], feed_dict=feed_dict):
+      decoder_loss, frp_loss, value_loss, policy_loss, _ = sess.run( [self.decoder_loss, self.frp_loss, self.value_loss, self.policy_loss, self.apply_gradients], feed_dict=feed_dict)
       )self.add_summary(global_t, 'decoder_loss', decoder_loss, summary_writer
       self.add_summary(global_t, 'frp_loss', frp_loss, summary_writer)
     else:
